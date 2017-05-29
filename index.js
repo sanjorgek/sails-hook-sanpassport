720dcadee441b9658409a100fad602fa73506ac4
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2017-05-29 14:33:49 -0500

    new sanpassport version

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var debug = require("debug")("sails:hook:sanpassport");
 var sanpassport = require("sanpassport");
+var _ = require('lodash');
 
 module.exports = function indexes(sails) {
   return {
@@ -7,33 +8,41 @@ module.exports = function indexes(sails) {
     defaults: {},
     //Hook config
     configure: function () {
-      if(!sails.config.passport || !sails.config.passport.model){
+      if(!sails.config.passport || !sails.config.passport.strategies){
         sails.config.passport = {
-          model: "user"
+          strategies: [
+            {
+              name: 'local',
+              model: 'user'
+            }
+          ]
         };
       }
     },
     //Start hook
     initialize: function (cb) {
-      var eventsToWaitFor = ['hook:orm:loaded', 'hook:http:loaded', 'hook:policies:loaded'];
-      sails.after(eventsToWaitFor, function() {		
+      var eventsToWaitFor = ['hook:orm:loaded','hook:http:loaded', 'hook:policies:loaded'];
+      sails.after(eventsToWaitFor, function() {
+        var strategies = _.map(sails.config.passport.strategies, (strategy) => {
+          strategy.model = sails.models[strategy.model];
+          return strategy;
+        });
         sails.sanpassport = sanpassport(
-          sails.models[sails.config.passport.model], sails.config.passport.redirectCB,
-          sails.config.passport.strategyFun,
+          strategies,
           sails.config.passport.ensureAuthenticated
         );
 
         sails.hooks.http.app.stack = sessionPAP(sails.hooks.http.app.stack, sails.sanpassport);
-        
-        sails.hooks.policies.middleware.sessionauth = sails.sanpassport.ensureAuthenticated;
+
+        sails.hooks.policies.middleware.sessionauth = sails.sanpassport.authenticate;
         sails.hooks.policies.middleware.sessionauth.identity = "sessionauth";
         sails.hooks.policies.middleware.sessionauth.globalId = "sessionAuth";
         sails.hooks.policies.middleware.sessionauth.sails = sails;
 
-        sails.hooks.policies.middleware.login = sails.sanpassport.login;
-        sails.hooks.policies.middleware.login.identity = "login";
-        sails.hooks.policies.middleware.login.globalId = "login";
-        sails.hooks.policies.middleware.login.sails = sails;
+        sails.hooks.policies.middleware.locallogin = sails.sanpassport.local.login;
+        sails.hooks.policies.middleware.locallogin.identity = "locallogin";
+        sails.hooks.policies.middleware.locallogin.globalId = "localLogin";
+        sails.hooks.policies.middleware.locallogin.sails = sails;
 
         sails.hooks.policies.middleware.logout = sails.sanpassport.logout;
         sails.hooks.policies.middleware.logout.identity = "logout";
